2a9aad3d64cdd89b6bdfcac90f38258058b38992
Amanda Medeiros <amandasmedeiros@outlook.com.br>
2024-05-05 17:56:08 -0300

    feat: Search component

--- flood-search/src/App.vue
+++ flood-search/src/App.vue
@@ -1,25 +1,36 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Search from './components/Search.vue';
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="wrapper">
+    <div class="header">
+      <h1 class="title">Lista de Abrigados</h1>
+      <p class="main-text">Pesquisa em compilado de listas de pessoas resgatadas nas enchentes do Rio Grande do Sul</p>
+      <Search />
+    </div>
+
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.title {
+  color: white;
+  font-weight: 600;
+  font-size: 30px;
+  line-height: 36px;
+  margin-top: 0;
+  margin-left:0;
+}
+.main-text{
+  color: white;
+}
+.wrapper {
+  background-color: #111827;
+  height: 100vh;
+  width: 100vw;
+  
 }
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
---
+++ flood-search/src/components/Search.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+  <h3 class="search-title">Nome completo</h3>
+  <UInput color="primary" variant="outline" placeholder="Search..." />
+</template>
+
+<style scoped>
+.search-title{
+  color: white;
+}
+</style>
