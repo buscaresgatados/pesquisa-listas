a06f117298a349ea2db86688353184ee346d812b
Pedro Vanzella <pedro@pedrovanzella.com>
2024-05-06 15:33:28 -0300

    Update api endpoint
    Signed-off-by: Pedro Vanzella <pedro@pedrovanzella.com>

--- flood-search/src/service/ResultService.js
+++ flood-search/src/service/ResultService.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const ResultService = {
   async search(termo) {
     try {
-      const response = await axios.get(`https://api.exemplo.com/busca?termo=${termo}`);
+      const response = await axios.get(`https://refugio-rs-prd-d5zml3w7fa-rj.a.run.app/pessoa?nome=${termo}`);
       return response.data;
     } catch (error) {
       throw new Error(`Erro ao buscar dados: ${error}`);
