b68723aafa56bd01b7361d924b6e0efcb1c77ccf
gabibranco <gabi@tatango.com>
2024-05-07 15:49:29 -0400

    Adds new spreadsheet to list

--- flood-search/src/components/LinkList.vue
+++ flood-search/src/components/LinkList.vue
@@ -32,6 +32,7 @@ import LinkComponent from './LinkComponent.vue';
             <LinkComponent url="https://docs.google.com/spreadsheets/u/0/d/1frgtJ9eK05OqsyLwOBiZ2Q6E7e4_pWyrb7fJioqfEMs/htmlview" name="RESGATADOS - Enchente RS"/>
             <LinkComponent url="https://docs.google.com/spreadsheets/d/1hxzHYE4UR1YbcH3ZQoPfcQTgPbRm5T6lShkDwGDoeXA/edit#gid=0" name="To Salvo Vale dos Sinos" />
             <LinkComponent url="https://docs.google.com/spreadsheets/d/1IjefUU8g5OaHxMfcvqc0ewyQDn5ZVLBE/edit#gid=747398008" name="ABRIGADOS EM GUAÍBA" tag title="NOVA"/>
+            <LinkComponent url="https://docs.google.com/spreadsheets/d/1f5gofOOv4EFYWhVqwPWbgF2M-7uHrJrCMiP7Ug4y6lQ/htmlview#gid=1619683963" name="LISTA DESABRIGADOS" tag title="NOVA"/>          
           </div>
         </div>
       </div>
