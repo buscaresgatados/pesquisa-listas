242eac63f0920c8cf4e7aeb2ee05f18746806e75
Pedro Vanzella <pedro@pedrovanzella.com>
2024-05-07 07:10:12 -0300

    Add vercel analytics
    Signed-off-by: Pedro Vanzella <pedro@pedrovanzella.com>

--- flood-search/src/main.js
+++ flood-search/src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
+import { inject } from '@vercel/analytics'
 import './style.css'
 import App from './App.vue'
 import router from './routes';
+
+inject()
 
 const app = createApp(App)
 
