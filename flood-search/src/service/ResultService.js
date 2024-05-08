c7b5243bf2f3d212e5f2219dc4f40f84defc8285
Pedro Vanzella <pedro@pedrovanzella.com>
2024-05-08 14:21:10 -0300

    frontend: use key
    Signed-off-by: Pedro Vanzella <pedro@pedrovanzella.com>

--- flood-search/src/service/ResultService.js
+++ flood-search/src/service/ResultService.js
@@ -4,7 +4,13 @@ import axios from 'axios';
 const ResultService = {
   async search(termo) {
     try {
-      const response = await axios.get(`https://refugio-rs-prd-d5zml3w7fa-rj.a.run.app/pessoa?nome=${termo}`);
+      const response = await axios.get(
+        `https://refugio-rs-prd-d5zml3w7fa-rj.a.run.app/pessoa?nome=${termo}`,
+        {
+          headers: {
+            Authorization: 'c2585727-bd1d-4b70-bd97-b0417c8e3c7c', // in `main.go`
+          }
+        });
       return response.data;
     } catch (error) {
       throw new Error(`Erro ao buscar dados: ${error}`);
