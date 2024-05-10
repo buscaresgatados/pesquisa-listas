1e99f6dcb0cc4e65b2657efbdb9cf57a55720ddb
Igor <igor.ecarrasco@gmail.com>
2024-05-10 09:11:35 -0400

    remove script

--- .github/workflows/Untitled-1.sh
+++
@@ -1,4 +0,0 @@
-REVISIONS=$(gcloud run revisions list --service=refugio-rs-stg --region=southamerica-east1 --format=json)
-PREVIOUS_REVISION=$(jq -r '.[1].metadata.name' <<< ${REVISIONS}) 
-NEW_REVISION=$(jq -r '.[0].metadata.name' <<< ${REVISIONS}) 
-gcloud run services update-traffic refugio-rs-stg --region=southamerica-east1 --to-revisions=$PREVIOUS_REVISION=100,$NEW_REVISION=0
