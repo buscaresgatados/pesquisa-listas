38ab1102ef032276ea8bbaa5bd5c2ae1526ca510
Lucas Trajano <lucast@tatango.com>
2024-05-11 15:02:16 -0300

    import

--- flood-search/src/components/ResultPage/Results/FoundResults.vue
+++ flood-search/src/components/ResultPage/Results/FoundResults.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ArrowTopRightOnSquareIcon } from '@heroicons/vue/24/solid'
+import { DocumentMagnifyingGlassIcon } from '@heroicons/vue/24/solid'
     defineProps({
     Nome: String,
     Idade: Number,
